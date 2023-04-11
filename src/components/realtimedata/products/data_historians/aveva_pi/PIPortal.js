c67a1008268b3dd6dd164e8584fe95c4751aa61d
Abhishek Chauhan <abhishek18chauhan@gmail.com>
2023-04-11 10:27:06 +0530

    SideBar commented coz gatsby deploy error

--- src/components/realtimedata/products/data_historians/aveva_pi/PIPortal.js
+++ src/components/realtimedata/products/data_historians/aveva_pi/PIPortal.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby";
 import AboutPI from "./AboutPI";
-import SideBar from "../../../../common_components/Sidebar/Sidebar.jsx";
-import "../../../../common_components/Sidebar/Sidebar.css";
+// import SideBar from "../../../../common_components/Sidebar/Sidebar.jsx";
+// import "../../../../common_components/Sidebar/Sidebar.css";
 import React from "react";
 import { Router } from "react-router-dom";
 
@@ -9,9 +9,9 @@ function PIPortal() {
   return (
     <div>
       <div class="main-container">
-        <div>
+        {/* <div>
           <SideBar />
-        </div>
+        </div> */}
         <div>
           <AboutPI />
         </div>
