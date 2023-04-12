036ac73fef4b5486b3ca89ad5d52956d009281fb
Abhishek Chauhan <abhishek18chauhan@gmail.com>
2023-04-12 20:39:23 +0530

    aveva_pi commented

--- src/components/realtimedata/products/data_historians/aveva_pi/AboutPI.js
+++ src/components/realtimedata/products/data_historians/aveva_pi/AboutPI.js
@@ -1,113 +1,113 @@
-import React from "react";
-function AboutPI() {
-  return (
-    <section id="aboutPI" class="aboutPI">
-      <div class="container  aos-init aos-animate" data-aos="fade-up">
-        <div class="row content whatIsPI">
-          <p>
-            {" "}
-            <h4>
-              <b>What is PI ?</b>
-            </h4>
-          </p>
+// import React from "react";
+// function AboutPI() {
+//   return (
+//     <section id="aboutPI" class="aboutPI">
+//       <div class="container  aos-init aos-animate" data-aos="fade-up">
+//         <div class="row content whatIsPI">
+//           <p>
+//             {" "}
+//             <h4>
+//               <b>What is PI ?</b>
+//             </h4>
+//           </p>
 
-          <p>
-            The PI System enables digital transformation through trusted,
-            high-quality operations data. Collect, enhance, and deliver data in
-            real time in any location. Empower engineers and operators.
-            Accelerate the work of analysts and data scientists. Support new
-            business opportunities.
-          </p>
-          <div class="col-lg-5">
-            <p>
-              {" "}
-              <b>PI suite of applications can provide:</b>
-            </p>
-            <ul>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Data collection
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Enhanced Data Storage
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Contextualization of Assets
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Advanced Visualization
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Equipment/Asset monitoring
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Advanced Analytics
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Manual Data Entry using mobile devices
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Integration with multiple interfaces
-              </li>
-            </ul>
+//           <p>
+//             The PI System enables digital transformation through trusted,
+//             high-quality operations data. Collect, enhance, and deliver data in
+//             real time in any location. Empower engineers and operators.
+//             Accelerate the work of analysts and data scientists. Support new
+//             business opportunities.
+//           </p>
+//           <div class="col-lg-5">
+//             <p>
+//               {" "}
+//               <b>PI suite of applications can provide:</b>
+//             </p>
+//             <ul>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Data collection
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Enhanced Data Storage
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Contextualization of Assets
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Advanced Visualization
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Equipment/Asset monitoring
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Advanced Analytics
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Manual Data Entry using mobile devices
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Integration with multiple interfaces
+//               </li>
+//             </ul>
 
-            <div class="justGap">{""}</div>
+//             <div class="justGap">{""}</div>
 
-            <p>
-              {" "}
-              <b>PI components can enable assets to do:</b>
-            </p>
-            <ul>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Remote Asset Surveillance
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Data Engineering
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Machines Learning
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Proactive Technical monitoring
-              </li>
-              <li>
-                {/* <i class="ri-check-double-line"></i> */}
-                Exception Based Surveillance
-              </li>
-            </ul>
+//             <p>
+//               {" "}
+//               <b>PI components can enable assets to do:</b>
+//             </p>
+//             <ul>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Remote Asset Surveillance
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Data Engineering
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Machines Learning
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Proactive Technical monitoring
+//               </li>
+//               <li>
+//                 {/* <i class="ri-check-double-line"></i> */}
+//                 Exception Based Surveillance
+//               </li>
+//             </ul>
 
-            <p>
-              For more details on OSIsoft PI, visit{" "}
-              <a href="https://www.osisoft.com/pi-system/" target="_blank">
-                AVEVA
-              </a>
-              's official website.
-            </p>
-          </div>
+//             <p>
+//               For more details on OSIsoft PI, visit{" "}
+//               <a href="https://www.osisoft.com/pi-system/" target="_blank">
+//                 AVEVA
+//               </a>
+//               's official website.
+//             </p>
+//           </div>
 
-          <div class="col-lg-7 pt-4 pt-lg-0">
-            {" "}
-            <img
-              src="assets/img/piservices_img1.gif"
-              class="img-fluid"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
+//           <div class="col-lg-7 pt-4 pt-lg-0">
+//             {" "}
+//             <img
+//               src="assets/img/piservices_img1.gif"
+//               class="img-fluid"
+//               alt=""
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
 
-export default AboutPI;
+// export default AboutPI;
