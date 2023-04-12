05fe64720c256288e572beb690de3cddf9cee835
Abhishek Chauhan <abhishek18chauhan@gmail.com>
2023-04-12 20:51:56 +0530

    put f=dev_ssr flag at right position

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,9 @@ module.exports = {
   siteMetadata: {
     title: "Shell Real-Time CC",
   },
+  flags: {
+    DEV_SSR: true,
+  },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-react-helmet",
@@ -24,9 +27,6 @@ module.exports = {
         theme_color: `#a2466c`,
         display: `standalone`,
         icon: `static/assets/images/common_images/shell_logo.png`,
-      },
-      flags: {
-        DEV_SSR: true,
       },
     },
   ],
